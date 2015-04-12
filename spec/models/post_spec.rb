d7d3d0be511768f60f504e1452bfcf85e1a505d1
Cari Westbrook <cari.westbrook@gmail.com>
2015-04-11 20:29:20 -0700

    add validations & tests for posts

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Post do
+  context 'validations' do
+    it { should validate_presence_of :title }
+    it { should validate_uniqueness_of :title}
+
+    it { should validate_presence_of :content }
+    it { should validate_uniqueness_of :content }
+  end
+end
