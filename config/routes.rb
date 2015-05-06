6c62259db3633bc089867a6c9b256852b472628e
Cari Westbrook <cari.westbrook@gmail.com>
2015-05-06 09:31:29 -0700

    add etsy route & view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :articles
 
+  get '/etsy'
+
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
