8fd37920dd3eb240b8093e958f2146db91bd693b
Cari Westbrook <cari.westbrook@gmail.com>
2015-05-19 23:14:58 -0700

    markdown parser will now parse code properly

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,19 +1,22 @@
 module ArticleHelper
-
-  def codify(text)
-    html = CodeRay.scan(text, :ruby).div(line_numbers: :table)
+  class CodeRayify < Redcarpet::Render::HTML
+    # overwrites the block_code method for RedCarpet so we can
+    # parse code blocks with a specific language
+    def block_code(code, language)
+      CodeRay.scan(code, :ruby).div(line_numbers: :table, hard_wrap: true)
+    end
   end
 
   def markdown(content)
+    code_parser = CodeRayify.new(filter_html: true, hard_wrap: true)
+
     options = {
+      fenced_code_blocks: true,
       disable_indented_clode_blocks: true,
-      autolink: true,
-      lax_spacing: true,
-      quote: true
+      autolink: true
     }
 
-    markdown = Redcarpet::Markdown.new(Redcarpet::Renderer::HTML, options)
-
+    markdown = Redcarpet::Markdown.new(code_parser, options)
     markdown.render(content).html_safe
   end
 
