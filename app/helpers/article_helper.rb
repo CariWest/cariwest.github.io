dc39486f7d2a2bd2fe26caf7a0f9e56f725abd7e
Cari Westbrook <cari.westbrook@gmail.com>
2015-05-19 22:18:42 -0700

    start the markdown editor parsing

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,2 +1,20 @@
 module ArticleHelper
+
+  def codify(text)
+    html = CodeRay.scan(text, :ruby).div(line_numbers: :table)
+  end
+
+  def markdown(content)
+    options = {
+      disable_indented_clode_blocks: true,
+      autolink: true,
+      lax_spacing: true,
+      quote: true
+    }
+
+    markdown = Redcarpet::Markdown.new(Redcarpet::Renderer::HTML, options)
+
+    markdown.render(content).html_safe
+  end
+
 end
