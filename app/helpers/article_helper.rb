a8ab2831c6b3973e4acfd2b6af5eb544ed3b55c5
Cari Westbrook <cari.westbrook@gmail.com>
2015-05-20 00:00:01 -0700

    refactor renderer

--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,23 +1,31 @@
 module ArticleHelper
+
   class CodeRayify < Redcarpet::Render::HTML
     # overwrites the block_code method for RedCarpet so we can
-    # parse code blocks with a specific language
-    def block_code(code, language)
-      CodeRay.scan(code, :ruby).div(line_numbers: :table, hard_wrap: true)
+    # parse code blocks with a specific language. Would love to make
+    # this use GitHub instead of CodeRay
+    def block_code(code, language = :ruby)
+      CodeRay.scan(code, language).div(line_numbers: :table, hard_wrap: true)
     end
   end
 
-  def markdown(content)
-    code_parser = CodeRayify.new(filter_html: true, hard_wrap: true)
+  def renderer
+    options = {
+      filter_html: true,
+      hard_wrap: true
+    }
 
+    CodeRayify.new(options)
+  end
+
+  def markdown(content)
     options = {
       fenced_code_blocks: true,
       disable_indented_clode_blocks: true,
       autolink: true
     }
 
-    markdown = Redcarpet::Markdown.new(code_parser, options)
+    markdown = Redcarpet::Markdown.new(renderer, options)
     markdown.render(content).html_safe
   end
-
 end
