13923b63c185b9b9448cd41c453bd0b006425d80
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-02 11:01:06 -0700

    add image model & associations to articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150414001619) do
+ActiveRecord::Schema.define(version: 20150602175822) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "excerpt"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.string   "img_url"
+    t.integer  "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
