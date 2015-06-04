4a1740668f57b8722cce004a95f4c2b769a8c83b
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-04 16:15:20 -0700

    migrate user table

--- db/migrate/20150604221230_create_users.rb
+++ db/migrate/20150604221230_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string  :username
       t.string  :password_hash
+      t.string  :email
       t.integer :login_count, default: 0
 
       t.timestamps null: false
