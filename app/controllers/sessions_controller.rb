f4db3b8f03e90464e9f39ffe79698bbeecb20a4a
Cari Westbrook <cari.westbrook@gmail.com>
2015-06-04 18:02:40 -0700

    transition sessions controller to use user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,25 @@
 class SessionsController < ApplicationController
-  include ApplicationHelper
 
   def new
-    @session = Session.new
   end
 
   def create
-    login = @session.login(params["username"], params["password"])
-
-    if !@login
-      not_found
-      @errors = ["User is not valid"]
+    if params["username"] == ENV["USERNAME"]
+      user = User.find_by(params["username"])
+      if user.authenticate(params["password"])
+        session[:id] = user.username
+      end
+    else
+      :unauthorized
+      @errors = ["User is not admin"]
     end
 
     redirect_to root_path
   end
 
   def delete
-    @session.logout
+    session[:id] = nil
+    @current_user = nil
   end
 
 end
